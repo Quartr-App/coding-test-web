94157463be3dff2b0d2685ae66ab0c60eb2f4256
Teddy <teodor.adolfsson@gmail.com>
2023-04-01 07:39:13 +0300

    restructure

--- __tests__/pages/home.test.tsx
+++ __tests__/pages/home.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Home from "@/pages/index";
+import Home from "@/app/page";
 
 describe("Home", () => {
   it("should render", async () => {
