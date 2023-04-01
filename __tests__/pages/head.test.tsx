27e1adb3764b2f19ec58b1aaaacb3a3f641e6d73
Teddy <teodor.adolfsson@gmail.com>
2023-04-01 07:30:33 +0300

    tests

---
+++ __tests__/pages/head.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Head from "app/head"; 
+
+describe("Head", () => {
+    it("should render", async () => {
+        render(<Head />);
+
+        const title = screen.getByRole("title");
+        expect(title).toHaveTextContent("Quartr");
+    });
+});
---
+++ __tests__/pages/home.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
+
+describe("Home", () => {
+  it("should render", async () => {
+    render(<Home />);
+
+    const heading = screen.getAllByRole("heading");
+    expect(heading[0]).toHaveTextContent("Quartr");
+    expect(heading[1]).toHaveTextContent("Trending companies");
+  });
+});
