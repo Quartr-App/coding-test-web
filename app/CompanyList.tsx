9b4c96e5dedceca9c79af7e32fb5aa0e2ec99039
Teddy <teodor.adolfsson@gmail.com>
2023-04-02 18:48:13 +0300

    remove unused code

--- app/CompanyList.tsx
+++ app/CompanyList.tsx
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React from "react";
 import { CompanyI, CompanyListItem } from "./Company";
 
 interface CompanyListProps {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import "@/app/styles/globals.css";
-import { Inter } from "@next/font/google";
-import { Suspense } from "react";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
