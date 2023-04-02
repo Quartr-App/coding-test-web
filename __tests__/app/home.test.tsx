13930d151717635baf9c29904108b2ff5c4224fa
Teddy <teodor.adolfsson@gmail.com>
2023-04-02 15:27:57 +0300

    tests wip

--- __tests__/app/home.test.tsx
+++ __tests__/app/home.test.tsx
@@ -1,12 +1,20 @@
-import Home from "@/app/page";
+import Home from "../../app/page";
 import { render, screen } from "@testing-library/react";
 
 describe("Home", () => {
-  it("should render", async () => {
-    render(<Home />);
+    it("Home renders correctly", async () => {
+        render(<Home />);
 
-    const heading = screen.getAllByRole("heading");
-    expect(heading[0]).toHaveTextContent("Quartr");
-    expect(heading[1]).toHaveTextContent("Trending companies");
-  });
+        const heading = screen.getAllByRole("heading");
+        expect(heading[0]).toHaveTextContent("Quartr");
+        expect(heading[1]).toHaveTextContent("Trending companies");
+    });
+    it("Connects to the API", async () => {
+        const fetchData = async () => {
+            const data_res = await fetch("/api/companies");
+            console.log("here is the " + {data_res});
+
+            expect(data_res.status).toBe(200);
+          };
+    });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,15 @@
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: "./",
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleNameMapper: {
-    // Handle module aliases
-    "^@(.*)$": "<rootDir>/$1",
-    "^@/app(.*)$": "<rootDir>/app/$1",
-    "^@/components/(.*)$": "<rootDir>/components/$1"
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 
-  },
-  testEnvironment: "jest-environment-jsdom",
+    testEnvironment: "jest-environment-jsdom",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
